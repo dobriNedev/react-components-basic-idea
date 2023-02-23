a7f632b603bef5aa75672602c3e9898db828517d
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-23 14:46:42 +0100

    added my first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
+import Title from './componets/Title';
+
 
 function App() {
   return (
     <div className="App">
      
+    <Title />
+
     </div>
   );
 }
---
+++ src/componets/Title.js
@@ -0,0 +1,7 @@
+const Title = () => {
+    return (
+        <h1>Testing componets and props</h1>
+    );
+};
+
+export default Title;

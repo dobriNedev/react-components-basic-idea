cf08f9dfedba454b155bb5fbdff368c9f7d18933
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-24 12:44:51 +0100

    added buttons container and styling

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
     <CalculatorContainer />
     <br></br>
     <Display />
-
     <IncrementBtn />
     <DecrementBtn />
     </div>
---
+++ src/componets/ButtonsContainer.js
@@ -0,0 +1,7 @@
+const ButtonContainer = ({ children }) => {
+    return(
+        <div className="btn-container">{children}</div>
+    );
+};
+
+export default ButtonContainer;

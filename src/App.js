9d0780e2e79f5f31bc8e6013bf450f255b3cdd81
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-24 13:17:09 +0100

    buttons values array added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+
 import './App.css';
 import Button from './componets/Button';
 import ButtonContainer from './componets/ButtonsContainer';
@@ -5,19 +7,13 @@ import CalculatorContainer from './componets/CalculatorContainer';
 
 import Display from './componets/Display';
 
-
-
 const App = () => {
   return (
-    
-     
-   
-
     <CalculatorContainer>
-      <Display value="0" />
-      <ButtonContainer>
-        <Button className=" " value="0" onClick={()=> {console.log("button clicked!");}} />
-      </ButtonContainer>
+        <Display value="0" />
+            <ButtonContainer>
+                <Button className=" " value="0" onClick={()=> {console.log("button clicked!");}} />
+            </ButtonContainer>
     </CalculatorContainer>
   );
 }
